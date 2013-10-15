53c8189371a6398671c4e2fd7592d5cada7a242a
Justin Karneges <justin@affinix.com>
2013-10-15 13:55:24 -0700

    fixes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def home(request):
+	return render_to_response('dashboard/home.html', {}, context_instance=RequestContext(request))
